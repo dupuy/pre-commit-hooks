2504b21a3c70536abd81efcd0ad91dd8c07f6030
Alexander Dupuy <alex.dupuy@mac.com>
2015-05-10 21:55:21 +0200

    refactor markdown linebreak extension testing
    use pytest.mark.parametrize to combine filename, input, output
    
    test that `txt` file is not considered as 'txt' extension
    test that `.txt` file is not considered as 'txt' extension
    
    The latter is the (correct) behavior of os.path.splitext(), and an example
    of why it is better to use the libraries than to mangle strings yourself.

--- tests/trailing_whitespace_fixer_test.py
+++ tests/trailing_whitespace_fixer_test.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import pytest
+
 from pre_commit_hooks.trailing_whitespace_fixer import fix_trailing_whitespace
 from testing.util import cwd
 
@@ -24,86 +26,91 @@ def test_fixes_trailing_whitespace(tmpdir):
             assert open(filename).read() == after_contents
 
 
-def test_fixes_trailing_markdown_whitespace(tmpdir):
-    with cwd(tmpdir.strpath):
-        for filename, contents in (
-                ('foo.md', 'foo  \nbar \n  '),
-                ('bar.markdown', 'bar   \nbaz\t\n\t\n'),
-        ):
-            with open(filename, 'w') as file_obj:
-                file_obj.write(contents)  # pragma: no branch (26 coverage bug)
+# filename, expected input, expected output
+# pylint: disable=bad-whitespace
+MD_TESTS_1 = (
+    ('foo.md',        'foo  \nbar \n  ',         'foo  \nbar\n\n'),
+    ('bar.Markdown',  'bar   \nbaz\t\n\t\n',     'bar  \nbaz\n\n'),
+    ('.md',           'baz   \nquux  \t\n\t\n',  'baz\nquux\n\n'),
+    ('txt',           'foo   \nbaz \n\t\n',      'foo\nbaz\n\n'),
+)
+# pylint: enable=bad-whitespace
 
-        ret = fix_trailing_whitespace(['foo.md', 'bar.markdown'])
+
+@pytest.mark.parametrize(('filename', 'input_s', 'output'), MD_TESTS_1)
+def test_fixes_trailing_markdown_whitespace(filename, input_s, output, tmpdir):
+    with cwd(tmpdir.strpath):
+        with open(filename, 'w') as file_obj:
+            file_obj.write(input_s)  # pragma: no branch (26 coverage bug)
+
+        ret = fix_trailing_whitespace([filename])
         assert ret == 1
-
-        for filename, after_contents in (
-                ('foo.md', 'foo  \nbar\n\n'),
-                ('bar.markdown', 'bar  \nbaz\n\n'),
-        ):
-            assert open(filename).read() == after_contents
+        assert open(filename).read() == output
 
 
-def test_markdown_linebreak_ext_opt(tmpdir):
+# filename, expected input, expected output
+# pylint: disable=bad-whitespace
+MD_TESTS_2 = (
+    ('foo.txt',       'foo  \nbar \n  \n',       'foo  \nbar\n\n'),
+    ('bar.MD',        'bar   \nbaz\t   \n\t\n',  'bar  \nbaz\n\n'),
+    ('.txt',          'baz   \nquux  \t\n\t\n',  'baz\nquux\n\n'),
+    ('txt',           'foo   \nbaz \n\t\n',      'foo\nbaz\n\n'),
+)
+# pylint: enable=bad-whitespace
+
+
+@pytest.mark.parametrize(('filename', 'input_s', 'output'), MD_TESTS_2)
+def test_markdown_linebreak_ext_opt(filename, input_s, output, tmpdir):
     with cwd(tmpdir.strpath):
-        for filename, contents in (
-                ('foo.txt', 'foo  \nbar \n  \n'),
-                ('bar.MD', 'bar   \nbaz\t   \n\t\n'),
-                ('bar.markdown', 'baz   \nquux  \t\n\t\n'),
-        ):
-            with open(filename, 'w') as file_obj:
-                file_obj.write(contents)  # pragma: no branch (26 coverage bug)
+        with open(filename, 'w') as file_obj:
+            file_obj.write(input_s)  # pragma: no branch (26 coverage bug)
 
         ret = fix_trailing_whitespace(['--markdown-linebreak-ext=md,TxT',
-                                       'foo.txt', 'bar.MD', 'bar.markdown'])
+                                       filename])
         assert ret == 1
-
-        for filename, after_contents in (
-                ('foo.txt', 'foo  \nbar\n\n'),
-                ('bar.MD', 'bar  \nbaz\n\n'),
-                ('bar.markdown', 'baz\nquux\n\n'),
-        ):
-            assert open(filename).read() == after_contents
+        assert open(filename).read() == output
 
 
-def test_markdown_linebreak_ext_opt_all(tmpdir):
+# filename, expected input, expected output
+# pylint: disable=bad-whitespace
+MD_TESTS_3 = (
+    ('foo.baz',       'foo  \nbar \n  ',         'foo  \nbar\n\n'),
+    ('bar',           'bar   \nbaz\t\n\t\n',     'bar  \nbaz\n\n'),
+)
+# pylint: enable=bad-whitespace
+
+
+@pytest.mark.parametrize(('filename', 'input_s', 'output'), MD_TESTS_3)
+def test_markdown_linebreak_ext_opt_all(filename, input_s, output, tmpdir):
     with cwd(tmpdir.strpath):
-        for filename, contents in (
-                ('foo.baz', 'foo  \nbar \n  '),
-                ('bar', 'bar   \nbaz\t\n\t\n'),
-        ):
-            with open(filename, 'w') as file_obj:
-                file_obj.write(contents)  # pragma: no branch (26 coverage bug)
+        with open(filename, 'w') as file_obj:
+            file_obj.write(input_s)  # pragma: no branch (26 coverage bug)
 
         # need to make sure filename is not treated as argument to option
         ret = fix_trailing_whitespace(['--markdown-linebreak-ext', '--',
-                                       'foo.baz', 'bar'])
+                                       filename])
         assert ret == 1
-
-        for filename, after_contents in (
-                ('foo.baz', 'foo  \nbar\n\n'),
-                ('bar', 'bar  \nbaz\n\n'),
-        ):
-            assert open(filename).read() == after_contents
+        assert open(filename).read() == output
 
 
-def test_no_markdown_linebreak_ext_opt(tmpdir):
+# filename, expected input, expected output
+# pylint: disable=bad-whitespace
+MD_TESTS_4 = (
+    ('bar.md',        'bar   \nbaz\t   \n\t\n',  'bar\nbaz\n\n'),
+    ('bar.markdown',  'baz   \nquux  \n',        'baz\nquux\n'),
+)
+# pylint: enable=bad-whitespace
+
+
+@pytest.mark.parametrize(('filename', 'input_s', 'output'), MD_TESTS_4)
+def test_no_markdown_linebreak_ext_opt(filename, input_s, output, tmpdir):
     with cwd(tmpdir.strpath):
-        for filename, contents in (
-                ('bar.md', 'bar   \nbaz\t   \n\t\n'),
-                ('bar.markdown', 'baz   \nquux  \t\n\t\n'),
-        ):
-            with open(filename, 'w') as file_obj:
-                file_obj.write(contents)  # pragma: no branch (26 coverage bug)
+        with open(filename, 'w') as file_obj:
+            file_obj.write(input_s)  # pragma: no branch (26 coverage bug)
 
-        ret = fix_trailing_whitespace(['--no-markdown-linebreak-ext',
-                                       'bar.md', 'bar.markdown'])
+        ret = fix_trailing_whitespace(['--no-markdown-linebreak-ext', filename])
         assert ret == 1
-
-        for filename, after_contents in (
-                ('bar.md', 'bar\nbaz\n\n'),
-                ('bar.markdown', 'baz\nquux\n\n'),
-        ):
-            assert open(filename).read() == after_contents
+        assert open(filename).read() == output
 
 
 def test_returns_zero_for_no_changes():
